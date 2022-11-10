9088c600f6c0ad0b1b7f8bd49bb0695629cb191d
augustocosta79 <amcneto@hotmail.com>
2022-11-10 16:57:39 -0300

    Add QuizzResult and correct imports

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,7 +1,7 @@
-import { Game } from "./components/quizzgame.js";
-import { QuizzLoader } from "./components/quizzloader.js";
-import { QuizzResult } from "./components/quizzresult.js";
-import { WindowCtrl } from "./components/windowctrl.js";
+import { Game } from "./components/quizzgame.js"
+import { QuizzLoader } from "./components/quizzloader.js"
+import { QuizzResult } from "./components/quizzresult.js"
+import { WindowCtrl } from "./components/windowctrl.js"
 
 new WindowCtrl()
 new QuizzLoader()
